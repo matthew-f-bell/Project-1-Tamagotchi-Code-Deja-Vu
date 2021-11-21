e4c46f461d3b31d8528422f2017efc5f6b963565
xdjx3runx <xdjx3runx@gmail.com>
2021-11-20 17:13:10 -0800

    Title screen created

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*----------------------------
+           Classes
+----------------------------*/
+class tamagotchi {
+    constructor(name) {
+        this.name = name;
+        this.hunger = 0;
+        this.sleepiness = 0;
+        this.boredom = 0;
+    }
+    death() {
+
+    }
+}
+/*----------------------------
+           Functions
+----------------------------*/
+const ageTamagotchi = function() {
+
+}
+
+/*----------------------------
+           Main Code
+----------------------------*/
+
+const $img = $('img');
+$img.attr("src", "assets/bedroom.jpeg");
